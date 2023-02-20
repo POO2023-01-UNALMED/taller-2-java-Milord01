d9aa4dc145012f75f32470cfc0d146e8c35aa378
Milord01 <124930730+Milord01@users.noreply.github.com>
2023-02-20 15:24:56 -0500

    clases

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -11,6 +11,34 @@ public Motor motor;
 public int registro;
 public static int cantidadCreados;
 
-Public void 
+public int cantidadAsientos() {
+	int a=0;
+	for(int i=0;i<asientos.length;i++) {
+		if (asientos[i]instanceof Asiento) {
+			a++;
+		}
+	}
+	return a;
+
+}
+
+public String verificarIntegridad() {
+    boolean estado = true;
+    if (motor.registro != this.registro) {
+        estado = false;
+    }
+    for (int i = 0; i< asientos.length; i++) {
+        if (asientos[i] != null) {
+            if (asientos[i].registro != this.registro) {
+                estado = false;
+            }
+        }
+    }
+    if (estado == true) {
+        return "Auto original";
+    }else{
+        return "Las piezas no son originales";
+    }
+}
 
 }
