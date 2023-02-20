e48f10d74580ed8664277ce75cb0a92957dadbd5
Milord01 <124930730+Milord01@users.noreply.github.com>
2023-02-20 14:57:48 -0500

    Clases

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Asiento {
+public String color;
+public int precio;
+public int registro;
+
+public void cambiarColor(String color) {
+	if(color=="rojo"||color=="verde"||color=="amarillo"||color=="negro"||color=="blanco") {
+		this.color=color;
+	}
+}
+}
---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Auto {
+	
+	
+public String modelo;
+public int precio;
+public Asiento[] asientos;
+public String marca;
+public Motor motor;
+public int registro;
+public static int cantidadCreados;
+
+Public void 
+
+}
